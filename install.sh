04b36610b2680b99235d9de184165bc621952629
readgovip <107192467+readgovip@users.noreply.github.com>
2025-11-17 19:30:12 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,66 +38,6 @@ arch() {
 
 echo "arch: $(arch)"
 
-os_version=""
-os_version=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
-
-if [[ "${release}" == "arch" ]]; then
-    echo "Your OS is Arch Linux"
-elif [[ "${release}" == "parch" ]]; then
-    echo "Your OS is Parch linux"
-elif [[ "${release}" == "manjaro" ]]; then
-    echo "Your OS is Manjaro"
-elif [[ "${release}" == "armbian" ]]; then
-    echo "Your OS is Armbian"
-elif [[ "${release}" == "opensuse-tumbleweed" ]]; then
-    echo "Your OS is OpenSUSE Tumbleweed"
-elif [[ "${release}" == "centos" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red} Please use CentOS 8 or higher ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "ubuntu" ]]; then
-    if [[ ${os_version} -lt 20 ]]; then
-        echo -e "${red} Please use Ubuntu 20 or higher version!${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "fedora" ]]; then
-    if [[ ${os_version} -lt 36 ]]; then
-        echo -e "${red} Please use Fedora 36 or higher version!${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "debian" ]]; then
-    if [[ ${os_version} -lt 11 ]]; then
-        echo -e "${red} Please use Debian 11 or higher ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "almalinux" ]]; then
-    if [[ ${os_version} -lt 9 ]]; then
-        echo -e "${red} Please use AlmaLinux 9 or higher ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "rocky" ]]; then
-    if [[ ${os_version} -lt 9 ]]; then
-        echo -e "${red} Please use Rocky Linux 9 or higher ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "oracle" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red} Please use Oracle Linux 8 or higher ${plain}\n" && exit 1
-    fi
-else
-    echo -e "${red}Your operating system is not supported by this script.${plain}\n"
-    echo "Please ensure you are using one of the following supported operating systems:"
-    echo "- Ubuntu 20.04+"
-    echo "- Debian 11+"
-    echo "- CentOS 8+"
-    echo "- Fedora 36+"
-    echo "- Arch Linux"
-    echo "- Parch Linux"
-    echo "- Manjaro"
-    echo "- Armbian"
-    echo "- AlmaLinux 9+"
-    echo "- Rocky Linux 9+"
-    echo "- Oracle Linux 8+"
-    echo "- OpenSUSE Tumbleweed"
-    exit 1
-fi
-
-
 install_base() {
     case "${release}" in
     centos | almalinux | rocky | oracle)
@@ -122,24 +62,12 @@ config_after_install() {
     echo -e "${yellow}Migration... ${plain}"
     /usr/local/s-ui/sui migrate
     
-    echo -e "${yellow}Install/update finished! For security it's recommended to modify panel settings ${plain}"
-    # read -p "Do you want to continue with the modification [y/n]? ": config_confirm
-    config_confirm="y"
+	$config_confirm="y"
     if [[ "${config_confirm}" == "y" || "${config_confirm}" == "Y" ]]; then
-        # echo -e "Enter the ${yellow}panel port${plain} (leave blank for existing/default value):"
-        # read config_port
-        config_port="65506"
-        # echo -e "Enter the ${yellow}panel path${plain} (leave blank for existing/default value):"
-        # read config_path
-        config_path="pp"
-
-        # Sub configuration
-        # echo -e "Enter the ${yellow}subscription port${plain} (leave blank for existing/default value):"
-        # read config_subPort
-        config_subPort="65507"
-        # echo -e "Enter the ${yellow}subscription path${plain} (leave blank for existing/default value):" 
-        # read config_subPath
-        config_subPath="sub"
+		$config_port=65506
+		$config_path="pp"
+		$config_subPort=65507
+		$config_subPath="sub"
 
         # Set configs
         echo -e "${yellow}Initializing, please wait...${plain}"
@@ -150,17 +78,13 @@ config_after_install() {
         [ -z "$config_subPath" ] || params="$params -subPath $config_subPath"
         /usr/local/s-ui/sui setting ${params}
 
-        # read -p "Do you want to change admin credentials [y/n]? ": admin_confirm
-        admin_confirm="y"
-        if [[ "${admin_confirm}" == "y" || "${admin_confirm}" == "Y" ]]; then
-            # First admin credentials
-            #read -p "Please set up your username:" config_account
-            config_account="admin"
-            #read -p "Please set up your password:" config_password
-            config_password="A88033054a"
 
+		$admin_confirm="y"
+        if [[ "${admin_confirm}" == "y" || "${admin_confirm}" == "Y" ]]; then
             # Set credentials
             echo -e "${yellow}Initializing, please wait...${plain}"
+			$config_account="admin"
+			$config_password="A88033054a"
             /usr/local/s-ui/sui admin -username ${config_account} -password ${config_password}
         else
             echo -e "${yellow}Your current admin credentials: ${plain}"
@@ -182,6 +106,21 @@ config_after_install() {
             echo -e "${red} this is your upgrade,will keep old settings,if you forgot your login info,you can type ${green}s-ui${red} for configuration menu${plain}"
         fi
     fi
+}
+
+prepare_services() {
+    if [[ -f "/etc/systemd/system/sing-box.service" ]]; then
+        echo -e "${yellow}Stopping sing-box service... ${plain}"
+        systemctl stop sing-box
+        rm -f /usr/local/s-ui/bin/sing-box /usr/local/s-ui/bin/runSingbox.sh /usr/local/s-ui/bin/signal
+    fi
+    if [[ -e "/usr/local/s-ui/bin" ]]; then
+        echo -e "###############################################################"
+        echo -e "${green}/usr/local/s-ui/bin${red} directory exists yet!"
+        echo -e "Please check the content and delete it manually after migration ${plain}"
+        echo -e "###############################################################"
+    fi
+    systemctl daemon-reload
 }
 
 install_s-ui() {
@@ -212,26 +151,26 @@ install_s-ui() {
 
     if [[ -e /usr/local/s-ui/ ]]; then
         systemctl stop s-ui
-        systemctl stop sing-box
     fi
 
     tar zxvf s-ui-linux-$(arch).tar.gz
     rm s-ui-linux-$(arch).tar.gz -f
 
-    wget --no-check-certificate -O /usr/bin/s-ui https://raw.githubusercontent.com/alireza0/s-ui/main/s-ui.sh
-
-    chmod +x s-ui/sui s-ui/bin/sing-box s-ui/bin/runSingbox.sh /usr/bin/s-ui
+    chmod +x s-ui/sui s-ui/s-ui.sh
+    cp s-ui/s-ui.sh /usr/bin/s-ui
     cp -rf s-ui /usr/local/
     cp -f s-ui/*.service /etc/systemd/system/
     rm -rf s-ui
 
     config_after_install
+    prepare_services
 
-    systemctl daemon-reload
     systemctl enable s-ui --now
-    systemctl enable sing-box --now
 
     echo -e "${green}s-ui v${last_version}${plain} installation finished, it is up and running now..."
+    echo -e "You may access the Panel with following URL(s):${green}"
+    /usr/local/s-ui/sui uri
+    echo -e "${plain}"
     echo -e ""
     s-ui help
 }
